e7e4e1dbeb0d2ddfdb27e4284f975ee12799c2a9
Gokul Krishna <iamgokul93@gmail.com>
2025-06-29 17:13:50 +0100

    test

--- debug.py
+++ debug.py
@@ -2,6 +2,8 @@ from src.student_performance.pipeline.training_pipeline import TrainingPipeline
 from dotenv import load_dotenv
 load_dotenv()
 
+
+
 if __name__ == "__main__":
     pipeline = TrainingPipeline()
     pipeline.run_pipeline()
